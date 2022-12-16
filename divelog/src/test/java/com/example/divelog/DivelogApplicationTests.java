0d62718c5abdb10106d1f706a1f8cab13e70fda1
JeongWonYoung <nicednjsdud@naver.com>
2022-12-16 22:38:39 +0900

    Build project

---
+++ divelog/src/test/java/com/example/divelog/DivelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.divelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
