556e9e687701889c9aa64114c78d0e28409724f5
JeongWonYoung <nicednjsdud@naver.com>
2023-02-09 23:02:02 +0900

    remove not need import

--- divelog/src/test/java/com/example/divelog/config/MyServicePropertiesTest.java
+++ divelog/src/test/java/com/example/divelog/config/MyServicePropertiesTest.java
@@ -1,13 +1,11 @@
 package com.example.divelog.config;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("my-service")
 @SpringBootTest
